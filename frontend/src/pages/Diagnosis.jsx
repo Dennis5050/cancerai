99b7191b2f450188dcf73fcafedd2252b88e7375
Deno5050 <deniskiplagat204@gmail.com>
2026-01-26 03:38:08 +1200

    endpoint update

--- frontend/src/pages/Diagnosis.jsx
+++ frontend/src/pages/Diagnosis.jsx
@@ -68,7 +68,7 @@ export default function Diagnosis() {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://cancerai.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
